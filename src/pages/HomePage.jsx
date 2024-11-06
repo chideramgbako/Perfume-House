7fdfe5c9e8d249f8493e0fca49ce3b5754fcf2fa
chidera mgbako <chideramgbako9@gmail.com>
2024-11-06 14:38:03 +0100

    error fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
-import React from "react"
+import React from "react";
 import Header from "../components/Header";
-import AllPerfumes from "../components/AllPerfumes";
+import AllPerfumes from "../components/Allperfumes";
 import Cart from "../components/Cart";
-
 
 const HomePage = () => {
   return (
     <>
-    <Header />
-      
+      <Header />
+
       <div className="container my-4">
         <div className="row">
           <div className="col-lg-8">
