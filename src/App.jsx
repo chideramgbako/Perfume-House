ce2c2b16aff3a044f32f1f0216207151ef048c7c
chidera mgbako <chideramgbako9@gmail.com>
2024-11-06 14:31:16 +0100

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+//new
 function App() {
   return (
     <>
